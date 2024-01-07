3ced395284f0e74c51f51549e61317b17d66c062
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-08 00:10:27 +0100

    create the register method in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterRequestInterface } from './types/registerRequest.interface';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { CurrentUserInterface } from './shared/types/currentUser.interface';
 import { envirponment } from '../../environments/environment';
+import { AuthResponseInterface } from './types/authResponse.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,9 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface>{
-    return this.http.post<CurrentUserInterface>(envirponment.apiURL+'',data);
+    return this
+            .http
+            .post<AuthResponseInterface>(envirponment.apiURL+'',data)
+            .pipe(map((response) => response.user));
   }
 }
