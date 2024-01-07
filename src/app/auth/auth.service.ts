c58038be314078f650e6152aa8c8e5b4b8ad4325
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-07 23:58:04 +0100

    create an envirment file to add the apiUrl globaly

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterRequestInterface } from './types/registerRequest.interface';
+import { Observable } from 'rxjs';
+import { CurrentUserInterface } from './shared/types/currentUser.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  register(data: RegisterRequestInterface): Observable<CurrentUserInterface>{
+    
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const envirponment={
+    production: false,
+    apiURL: 'http://localhost:8080/api/v1/'
+}
