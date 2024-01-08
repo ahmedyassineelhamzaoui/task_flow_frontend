ebe5c26206a84085ca524d4837998bf84e61498a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-08 01:05:28 +0100

    update action file to to use three actions

--- src/app/auth/store/action.ts
+++ src/app/auth/store/action.ts
@@ -1,7 +1,28 @@
-import { createAction, props } from "@ngrx/store";
+import { createAction, createActionGroup, emptyProps, props } from "@ngrx/store";
 import { RegisterRequestInterface } from "../types/registerRequest.interface";
+import { CurrentUserInterface } from "../shared/types/currentUser.interface";
 
-export const register = createAction(
-    '[Auth] Register'
-    ,props<{request: RegisterRequestInterface}>()
-);
+
+export const authActions = createActionGroup({
+
+    source : 'auth',
+    events : {
+        Register: props<{request: RegisterRequestInterface}>(),
+        'Register Success': props<{request: CurrentUserInterface}>(),
+        'Register Failure': emptyProps(),
+    }
+})
+
+
+// export const register = createAction(
+//     '[Auth] Register'
+//     ,props<{request: RegisterRequestInterface}>()
+// );
+// export const registerSuccess = createAction(
+//     '[Auth] Register Success'
+//     ,props<{request: RegisterRequestInterface}>()
+// );
+// export const registerFailure = createAction(
+//     '[Auth] Register Failure'
+//     ,props<{request: RegisterRequestInterface}>()
+// );
