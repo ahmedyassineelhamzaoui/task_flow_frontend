f2b3431c7bf6ab042b8624535a5f7055b6283bc2
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-09 09:24:05 +0100

    add formControl and display all form values in console

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import { selectIsSubmitting, selectValidationErrors } from '../../store/reducer';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  data$ = combineLatest({
+    isSubmiting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  })
+  constructor(private store:Store,
+              private fb:FormBuilder) { }
+  formLogin = this.fb.group({
+      email : ['',Validators.required,Validators.email],
+      password : ['',Validators.required]
+  });
+
+  signIn(){
+    console.log(this.formLogin.getRawValue());
+  }
+
+  getFirstError(errors: any): string {
+    const firstKey = Object.keys(errors)[0];
+    return errors[firstKey];
+  }
 }
