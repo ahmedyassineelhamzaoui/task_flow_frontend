9428bf7c1859be63f34453ecc9514d32abb25429
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-09 22:30:24 +0100

    create the getAllTasks method

--- src/app/dashboard-layout/components/task/task.component.ts
+++ src/app/dashboard-layout/components/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadTasks } from '../../store/action';
 
 @Component({
   selector: 'app-task',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
 
+  constructor(private store: Store) {}
+  ngOnInit() {
+      this.store.dispatch(loadTasks());
+      console.log("call get all tasks")
+  }
 }
--- src/app/dashboard-layout/services/task.service.ts
+++ src/app/dashboard-layout/services/task.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TaskResponseInterface } from '../types/task-response.interface';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllTasks(): Observable<TaskResponseInterface[]> {
+    return this.http.get<TaskResponseInterface[]>(environment.apiURL + '/tasks');
+  }
+  
 }
