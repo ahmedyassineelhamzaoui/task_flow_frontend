6e569255ee24adb6c7cd969b352a84628e22576e
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-09 22:04:10 +0100

    create user response interface

---
+++ src/app/dashboard-layout/types/user-response.interface.ts
@@ -0,0 +1,8 @@
+export interface UserResponseInterface {
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    modificationCredit: number,
+    deletionCredit: number
+}
