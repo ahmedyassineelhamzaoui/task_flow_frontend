bfc72880d05b60807d53abf71a888bf703a9f92c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-09 09:25:42 +0100

    add login action

--- src/app/auth/store/action.ts
+++ src/app/auth/store/action.ts
@@ -14,6 +14,15 @@ export const authActions = createActionGroup({
     }
 })
 
+export const loginActions = createActionGroup({
+    source: 'login',
+    events :{
+        Login: props<{request: LoginRequestInterface}>(),
+        'Login Success': props<{currentUser: CurrentUserInterface}>(),
+        'Login Failure': props<{errors: BackendErrorInterface}>(),
+    }
+});
+
 
 // export const register = createAction(
 //     '[Auth] Register'
