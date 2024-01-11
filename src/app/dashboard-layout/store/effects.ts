a6059e07fe89e10cfa35fa6ce5f8e3e4f3cb0eba
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-11 22:01:45 +0100

    update effects content

--- src/app/dashboard-layout/store/effects.ts
+++ src/app/dashboard-layout/store/effects.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { loadTasks, loadTasksSuccess, loadTasksFailure } from './action';
 import { catchError, map, mergeMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { TaskService } from '../services/task.service';
 
 @Injectable()
-export class TaskEffects{
+export class TasksEffects{
     constructor(
         private actions$: Actions,
         private taskService:TaskService
     ){}
 
-    loadTasks$ = createEffect(() => this.actions$.pipe(
-        ofType(loadTasks),
-        mergeMap(() => this.taskService.getAllTasks()
-        .pipe(
-            map(tasks => loadTasksSuccess({ tasks})),
-            catchError(error => of(loadTasksFailure({ error })))
+    getAllTasksEffect=createEffect(()=>this.actions$.pipe(
+        ofType('[Task] Get All Tasks'),
+        mergeMap((action)=>this.taskService.getAllTasks().pipe(
+            map((tasks)=>{
+                return {type:'[Task] Get All Tasks success',payload:tasks}
+            }),
+            catchError((error)=>{
+                return of({type:'[Task] Get All Tasks failure',payload:error})
+            })
         ))
-    ));
+    ))
+   
 }
