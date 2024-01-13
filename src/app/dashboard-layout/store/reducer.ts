d2d4e02eb085d22d78d8771b0a48c9dbcd493344
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-13 21:02:00 +0100

    add all task action into task reducer

--- src/app/dashboard-layout/store/reducer.ts
+++ src/app/dashboard-layout/store/reducer.ts
@@ -19,8 +19,9 @@ const initialState: TaskStateInterface = {
     errorMessage: '',
     dataSate: TaskStateEnum.INTILALE
 }
-export function tasksReducer(state: TaskStateInterface = initialState, action: Action){
+export function tasksReducer(state: TaskStateInterface = initialState, action: Ac   tion){
     switch(action.type){
+        // get all tasks
         case '[Task] Get All Tasks':
             return {
                 ...state,
@@ -33,6 +34,24 @@ export function tasksReducer(state: TaskStateInterface = initialState, action: A
                 dataSate: TaskStateEnum.SUCCESS
             }
         case '[Task] Get All Tasks failure':
+            return {
+                ...state,
+                errorMessage: (<TasksActions>action).payload,
+                dataSate: TaskStateEnum.ERROR
+            }
+        // search tasks
+        case '[Task] Search Tasks':
+            return {
+                ...state,
+                dataSate: TaskStateEnum.LOADING
+            }
+        case '[Task] Search Tasks success':
+            return {
+                ...state,
+                tasks: (<TasksActions>action).payload,
+                dataSate: TaskStateEnum.SUCCESS
+            }
+        case '[Task] Search Tasks failure':
             return {
                 ...state,
                 errorMessage: (<TasksActions>action).payload,
