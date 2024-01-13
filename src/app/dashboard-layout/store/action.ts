cabaa97918bf196c9b285285948177eac695e6e0
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-13 20:58:51 +0100

    add all actions for search

--- src/app/dashboard-layout/store/action.ts
+++ src/app/dashboard-layout/store/action.ts
@@ -2,6 +2,7 @@ import { Action } from "@ngrx/store";
 import { TaskResponseInterface } from "../types/task-response.interface";
 import { TaskStateInterface } from "./reducer";
 
+// get all tasks actions
 export class GetAllTasksAction implements Action {
    type = '[Task] Get All Tasks';
    constructor(public payload: any) { }
@@ -14,4 +15,19 @@ export class GetAllTasksFailureAction implements Action {
     type = '[Task] Get All Tasks failure';
     constructor(public payload: string) { }
 }
+
+// search actions
+export class SearchTasksAction implements Action {
+    type = '[Task] Search Tasks';
+    constructor(public payload: any) { }
+}
+export class SearchTasksSuccessAction implements Action {
+    type = '[Task] Search Tasks success';
+    constructor(public payload: TaskStateInterface[]) { }
+}
+export class SearchTasksFailureAction implements Action {
+    type = '[Task] Search Tasks failure';
+    constructor(public payload: string) { }
+}
+
 export type TasksActions = GetAllTasksAction | GetAllTasksSuccessAction | GetAllTasksFailureAction;
